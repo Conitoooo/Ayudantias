342fdc8e88fa4147ac38d501826a4103a52adf5e
Angel <136751632+arkhadaa@users.noreply.github.com>
2024-10-06 21:59:43 -0300

    Cambio ruta estilo.css

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db'); // Importa tu conexión a la base de datos
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // Para manejar JSON en las solicitudes
+
+const PORT = process.env.PORT || 3000;
+
+// Inicia el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
